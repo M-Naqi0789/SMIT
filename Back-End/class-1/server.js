af8699afe61a9ec355e7ce03010da475da2bf3c3
Mohammad Saad Ali <mohammadsaadali786.92@gmail.com>
2026-01-01 09:48:42 +0500

    fetch all users api created

--- Back-End/class-1/server.js
+++ Back-End/class-1/server.js
@@ -7,6 +7,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { config } from "dotenv";
+import users from "./src/dummy-data/dummy-data.js";
 
 // Environment variables config...!
 config({
@@ -25,9 +26,37 @@ app.use(cors());
 // Create 1st api: / route...!
 app.get("/", (req, res) => {
   return res.status(200).send({
-    statusCode : 200,
-    message : 'Welcome to Back End using Node JS'
+    statusCode: 200,
+    message: 'Welcome to Back End using Node JS'
   });
+});
+
+// Create 2nd api: /users route(Feth all users)...!
+app.get("/users", (req, res) => {
+  try {
+    // 400:
+    if (users.length < 1) {
+      return res.status(400).send({
+        status: false,
+        message: "No data found"
+      });
+    };
+
+    // 200:
+    return res.status(200).send({
+      status: true,
+      message: "Users list fetched successfully",
+      data: users
+    });
+  }
+
+  catch (error) {
+    // 500:
+    return res.status(500).send({
+      status: false,
+      message: "Something went wrong from server side"
+    });
+  }
 });
 
 // Server running...!
---
+++ Back-End/class-1/src/dummy-data/dummy-data.js
@@ -0,0 +1,6 @@
+// This is dummy json dta file...!
+
+const users = [
+];
+
+export default users;
