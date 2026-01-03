e88bef8d0786eb98f2c8bc1709b7bd888c4fe7bc
hasanCoder66 <hasanashraf874@gmail.com>
2026-01-03 10:54:41 +0500

    Full stack todo almost completed

--- Back-End/class-1/server.js
+++ Back-End/class-1/server.js
@@ -11,24 +11,24 @@ import users from "./src/dummy-data/dummy-data.js";
 
 // Environment variables config...!
 config({
-  path: './.env'
+  path: "./.env",
 });
 
 // Global variables...!
 const port = process.env.PORT;
 const app = express();
-const todoBucket = [];
+let todoBucket = [];
 
 // Middlewares...!
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 
 // Create 1st api: / route...!
 app.get("/", (req, res) => {
   return res.status(200).send({
     statusCode: 200,
-    message: 'Welcome to Back End using Node JS'
+    message: "Welcome to Back End using Node JS",
   });
 });
 
@@ -39,23 +39,21 @@ app.get("/users", (req, res) => {
     if (users.length < 1) {
       return res.status(400).send({
         status: false,
-        message: "No data found"
+        message: "No data found",
       });
-    };
+    }
 
     // 200:
     return res.status(200).send({
       status: true,
       message: "Users list fetched successfully",
-      data: users
+      data: users,
     });
-  }
-
-  catch (error) {
+  } catch (error) {
     // 500:
     return res.status(500).send({
       status: false,
-      message: "Something went wrong from server side"
+      message: "Something went wrong from server side",
     });
   }
 });
@@ -70,26 +68,59 @@ app.post("/todo/add", (req, res) => {
     if (!todoValue) {
       return res.status(400).send({
         status: false,
-        message: "Todo value is required."
+        message: "Todo value is required.",
       });
-    };
+    }
 
     // 200:
     const fetchTodos = [...todoBucket];
     fetchTodos.push(todoValue);
+    todoBucket = fetchTodos;
+
     return res.status(200).send({
       status: true,
-      message: "Todo added successfully"
+      message: "Todo added successfully",
     });
-  }
-
-  catch (error) {
+  } catch (error) {
     // 500:
     return res.status(500).send({
       status: false,
-      message: "Server us not working!"
+      message: "Server us not working!",
     });
   }
+});
+
+// Create 4th api: /todo/fetch-all route(Fetch all todo data from DB)...!
+app.get("/todo/fetch-all", (req, res) => {
+  try {
+    // 400:
+    if (todoBucket.length < 1) {
+      return res.status(400).send({
+        status: false,
+        message: "No data found",
+      });
+    }
+
+    // 200:
+    const todos = [...todoBucket];
+    return res.status(200).send({
+      status: true,
+      message: "Todo list fetched successfully",
+      data: todos,
+    });
+  } catch (error) {
+    // 500:
+    return res.status(500).send({
+      status: false,
+      message: "Something went wrong from server side",
+    });
+  }
+});
+
+// Create 5th api: /todo/delete/:key route(Delete todo data from DB)...!
+app.delete("/todo/delete/:key", (req, res) => {
+  const { key } = req.params;
+  console.log(`Todo iten key: ${key}`);
 });
 
 // Server running...!
--- Next-JS/nextjs-revision/app/home/page.tsx
+++ Next-JS/nextjs-revision/app/home/page.tsx
@@ -1,35 +1,85 @@
 // Home Screen...!
 
-'use client';
+"use client";
 
-import React, { useEffect } from 'react';
-import Header from '../src/components/header/header';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [todoInput, setTodoInput] = useState("");
 
-  const apiCall = async () => {
-    console.log('console is working!');
-    
-    const apiUrl = "http://192.168.24.200:3000/";
+  // Get Data...!
+  const handleGetTodoData = async () => {
+    try {
+      const apiUrl = "http://localhost:5050/todo/fetch-all";
+      const res = await axios.get(apiUrl);
+      console.log("Fetch todos data: ", res);
+      const { status, data } = res;
+
+      if (status) {
+        setTodos(data?.data || []);
+      }
+    } catch (error) {
+      console.log("Err while fetching todos: ", error);
+    }
+  };
+
+  // Post Data...!
+  const handleAddTodo = async () => {
+    const obj = { todoValue: todoInput.trim() };
+    console.log("Obj: ", obj);
 
     try {
-      const res = await fetch(apiUrl);
-      const jsonData = await res?.json();
-      console.log('Api res: ', jsonData);
+      const res = await axios({
+        method: "POST",
+        url: "http://localhost:5050/todo/add",
+        data: obj,
+      });
+      console.log("Post api res: ", res);
+      const { status, data } = res;
+      if (status) {
+        setTodoInput("");
+        handleGetTodoData();
+      }
+    } catch (error) {
+      console.log("Err while adding todo: ", error);
     }
+  };
 
-    catch (error) {
-      console.log('Err in api integration: ', error);
-    }
-  }
+  // Delete Data...!
+  const handleDelete = (key: number) => {
+    console.log("Li to delete: ", key);
+  };
 
+  // Mounted hook...!
   useEffect(() => {
-    apiCall();
+    handleGetTodoData();
   }, []);
 
   return (
     <div>
-      <Header screenName='Home' />
+      <h1> Full Stack Todo App </h1>
+      <input
+        type="text"
+        placeholder="Add Todo"
+        value={todoInput}
+        onChange={(e) => setTodoInput(e.target.value)}
+      />
+      <button onClick={handleAddTodo}> Add Item </button>
+
+      {todos.length < 1 && <h1> No Data Found </h1>}
+
+      <ul>
+        {todos?.map((item: string, index: number) => {
+          return (
+            <li key={index}>
+              {item}
+              <button onClick={() => handleDelete(index)}> Delete Item </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
